718b06007c8e10c9ed1c788788567d5b373ce18e
its-debbiecodes <debbieabegunde23@gmail.com>
2026-07-02 21:35:39 +0100

    git commit -m "Added done option to to-do list"

--- to-do-list-v0.5.py
+++ to-do-list-v0.5.py
@@ -1,9 +1,5 @@
 
-title_text="My to do list".title().center(100)
-print(f"\033[32m{title_text}\033[0m")
-print("What would you like to do?")
 
-my_list=[]
 
 
 title= "Welcome to the To-Do List"
@@ -13,7 +9,7 @@ to_do_list= []
 while True:
     choice_1="Add tasks to To-Do List (press a)"
     choice_2="View to To-Do List (press v)"
-    choice_3="Mark tasks as done (press m)"
+    choice_3="Mark tasks as ✅ (press m)"
     choice_4="Delete task (press d)"
     print(f"\n{choice_1} \n{choice_2} \n{choice_3} \n{choice_4}")
     users_choice= input("What would you like to do? ")
@@ -26,15 +22,36 @@ while True:
                 print("\n\033[35mTask added!\033[0m")
                 break
             else:
-                to_do_list.append(task)
+                to_do_list.append({"task" : task, "✅" : False})
 
     elif users_choice == "v":
+
         if len(to_do_list) > 0:
             list_title= "To-Do List"
-            print(f"\n\033[42m{list_title}\033[0m")
+            print(f"\n\033[43m{list_title}\033[0m")
 
             for i, task in enumerate(to_do_list, start=1):
                 print(f"\033[32m{i}. {task}\033[0m")
 
         else:
-            print("\n\033[44mYou have no tasks added\033[0m")
+            print("\n\033[43vmYou have no tasks added\033[0m")
+
+    elif users_choice == "m":
+        if len(to_do_list) > 0:
+            for i, task in enumerate(to_do_list, start=1):
+                print(f"\033[32m{i}. {task}\033[0m")
+            print("What tasks number would you like to mark as ✅ (enter f when done)")
+            while True:
+                mark_done = input("Enter tasks number: ")
+                if mark_done == "f":
+                    print("Tasks done ✅. Good job!")
+                    break
+                else:
+                    task_index = int(mark_done) - 1
+                    to_do_list[task_index]["✅"]= True
+
+        else:
+            print("\n\033[44mYou have no tasks in you To-do List\033[0m")
+
+
+
