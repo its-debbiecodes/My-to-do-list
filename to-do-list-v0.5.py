9c31cd5e9496b8baf82d2c50f49cf17a7a23b88f
its-debbiecodes <debbieabegunde23@gmail.com>
2026-07-02 23:23:41 +0100

    git commit -m "Added error handling to delete and done option"

--- to-do-list-v0.5.py
+++ to-do-list-v0.5.py
@@ -8,11 +8,12 @@ while True:
     choice_2="View to To-Do List (press v)"
     choice_3="Mark tasks as ✅ (press m)"
     choice_4="Delete task (press d)"
-    print(f"\n{choice_1} \n{choice_2} \n{choice_3} \n{choice_4}")
-    users_choice= input("What would you like to do? ")
+    choice_5="Exit (press e)"
+    print(f"\n{choice_1} \n{choice_2} \n{choice_3} \n{choice_4} \n{choice_5}")
+    users_choice= input("\nWhat would you like to do? ")
 
     if users_choice == "a":
-        print("Please enter your tasks (press e when done)")
+        print("\nPlease enter your tasks (press f when done)")
         while True:
             task = input("Enter Tasks: ")
             if task == "f":
@@ -35,7 +36,7 @@ while True:
                     print(f"\033[32m{i}. {task['task']}\033[0m")
 
         else:
-            print("\n\033[43vmYou have no tasks added\033[0m")
+            print("\n\033[42mYou have no tasks added\033[0m")
 
     elif users_choice == "m":
         if len(to_do_list) > 0:
@@ -49,26 +50,41 @@ while True:
                     print("Tasks done ✅. Good job!")
                     break
                 else:
-                    task_index = int(mark_done) - 1
-                    to_do_list[task_index]["✅"]= True
-
+                    try:
+                        mark_index = int(mark_done) - 1
+                        if mark_index< 0 or mark_index > len(to_do_list):
+                            print("\033[31mTask number doesn't exist\033[0m")
+                        else:
+                            task_index = int(mark_done) - 1
+                            to_do_list[task_index]["✅"]= True
+                    except ValueError:
+                        print("\033[31mPlease enter a valid task number!\033[0m")
         else:
-            print("\n\033[44mYou have no tasks in you To-do List\033[0m")
+            print("\n\033[42mYou have no tasks in you To-do List\033[0m")
 
     elif users_choice == "d":
         if len(to_do_list) > 0:
-            for i, task in enumerate(to_do_list, start=1):
-                print(f"\033[32m{i}. {task['task']}\033[0m")
 
             print("What tasks number would you like to delete (enter f when done)")
             while True:
+                for i, task in enumerate(to_do_list, start=1):
+                    print(f"\033[32m{i}. {task['task']}\033[0m")
                 delete_task = input("Enter tasks number: ")
                 if delete_task == "f":
                     print("Great! Tasks deleted!")
                     break
                 else:
-                    task_index = int(delete_task) - 1
-                    to_do_list.pop(task_index)
+                    try:
+                        task_index = int(delete_task) - 1
+                        if task_index < 0 or task_index >= len(to_do_list):
+                            print("\033[31mTask number doesn't exist\033[0m")
+
+                        else:
+                            to_do_list.pop(task_index)
+                            print("task deleted!")
+
+                    except ValueError:
+                        print("\033[31mPlease enter a valid task number!\033[0m")
         else:
             print("\n\033[42mYou have no tasks in you To-do List\033[0m")
 
